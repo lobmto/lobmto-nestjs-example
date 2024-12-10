f14827fe33aab4708f184537e8e5d7921b2b0e4c
lobmto <lobmto@gmail.com>
2024-12-10 23:12:06 +0900

    feat: ORM を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'main',
+      entities: [],
+    }),
+  ],
   controllers: [HealthController],
   providers: [HealthService],
 })
