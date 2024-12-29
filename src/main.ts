a47ea143211405513bd14439205cab6773292dd3
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2024-12-29 16:07:12 +0900

    feat: エラーレスポンスを統一

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exception-filters/http-error.filter';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap(): Promise<void> {
       forbidNonWhitelisted: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
